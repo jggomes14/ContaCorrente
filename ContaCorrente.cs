e3c4963d5809219d0d2885a20a1384466cdfc295
jggomes14 <jggomes141097@gmail.com>
2023-04-10 10:44:00 -0300

    Adicionar arquivos de projeto.

---
+++ ContaCorrente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente
+{
+    internal class ContaCorrente
+    {
+       public int Saldo { get; set; }
+       public int Numero { get; set; }
+       public int Limite { get; set; }
+       public bool EhEspecial { get; set; }
+        
+
+
+
+        public ContaCorrente(int saldo, int numero, int limite, bool ehEspecial)
+        {
+            Saldo = saldo;
+            Numero = numero;
+            Limite = limite;
+            EhEspecial = ehEspecial;
+            
+        }
+        public void Sacar(int valor)
+        {
+            Saldo = Saldo - valor;
+            Console.WriteLine("Saque Realizado!");
+
+        }
+        public void Depositar(int valor)
+        {
+            Saldo = Saldo + valor;
+
+            Console.WriteLine("Depósito realizado!");
+
+        }
+        public void TransferirPara(ContaCorrente destino, decimal valor)
+        {
+
+        }
+        public void ExibirExtrato()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace ContaCorrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente(1000, 12, 0, true);
+
+            
+
+            conta1.Sacar(200);
+            conta1.Depositar(300);
+            conta1.Depositar(500);
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente(300, 13, 0, true);
+         
+            
+
+            conta1.TransferirPara(conta2, 400);
+
+            conta1.ExibirExtrato();
+
+            Console.ReadKey();
+        }
+    }
+}
